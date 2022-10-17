868e37ccba2ca870fbf4389416f4829958d9db75
Pedro Ribas <pedroibribas@gmail.com>
2022-10-17 15:48:46 -0400

    feature: add cors

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
-const dotenv = require("dotenv").config();
 const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require("dotenv").config();
 const { getDonations, addDonator, deleteDonation, setDonations, deleteDonator } = require("./src/controllers/donation");
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
