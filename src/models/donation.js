2ba3e3fd87886f227cdd91e9f3b3df637fb91852
Pedro Ribas <pedroibribas@gmail.com>
2022-10-17 11:52:03 -0400

    feature: update models

--- src/models/donation.js
+++ src/models/donation.js
@@ -2,18 +2,9 @@ const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 
-const donatorSchema = new Schema({
-  name: String,
-  phone: String
-});
-
 const donationSchema = new Schema({
-  donation: String,
-  amount: {
-    type: Number,
-    default: 1
-  },
-  donator: donatorSchema
+  title: String,
+  donator: String
 });
 
 module.exports = mongoose.model("Donation", donationSchema);
